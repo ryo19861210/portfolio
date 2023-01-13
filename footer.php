ce2e403af919907d84b78154a8e672427ec41c4a
ryo19861210 <101373387+ryo19861210@users.noreply.github.com>
2023-01-13 16:08:04 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer id="footer">
+    <div class="footer_menu">
+      <nav class="footer_nav">
+        <a href="<?php echo home_url(); ?>/page/about">About</a>
+        <a href="<?php echo home_url(); ?>#works">Works</a>
+        <a href="<?php echo home_url(); ?>/page/contact">Contact</a>
+      </nav>
+      <p class="copy">
+        &copy; All right reserved.
+      </p>
+    </div>
+  </footer>
+  <!-- <script src="<?php echo get_template_directory_uri(); ?>/https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script> -->
+  <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+  <?php wp_footer(); ?>
+</body>
+
+</html>
